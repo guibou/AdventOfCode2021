c30945f83e0d461cb13345aa6ac8a92f60a738ac
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2021-12-12 12:22:14 +0100

    Use flake.nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "AoC 2021";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils}:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in
+      with pkgs.haskell.lib;
+      rec {
+        defaultPackage = (pkgs.haskellPackages.developPackage
+          {
+            root = ./.;
+            overrides = self: super:
+              {
+                PyF = unmarkBroken (dontCheck super.PyF);
+                besout = unmarkBroken (doJailbreak super.besout);
+              };
+          });
+        devShell = defaultPackage.overrideAttrs (old: {
+          buildInputs = [ pkgs.cabal-install pkgs.haskell-language-server ];
+        });
+      });
+}
