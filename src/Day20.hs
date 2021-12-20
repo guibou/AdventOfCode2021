391b99a6ad38e6e20728a242a406a96928d1abaa
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2021-12-20 19:46:59 +0100

    Day20: Rewrite the structure to a Set, speed x3

--- src/Day20.hs
+++ src/Day20.hs
@@ -4,11 +4,12 @@
 -- second star immediatly after
 module Day20 where
 
+import Control.Lens
+import Data.Bits
 import qualified Data.Map as Map
+import qualified Data.Set as Set
 import qualified Data.Text as Text
 import Utils
-import Control.Lens
-import qualified Data.Vector.Unboxed as Vector
 
 fileContent :: _
 fileContent = parseContent $(getFile)
@@ -16,37 +17,53 @@ fileContent = parseContent $(getFile)
 parseContent :: Text -> _
 parseContent t =
   let (Text.replace "\n" "" -> algo, Text.drop 2 -> image) = Text.breakOn "\n\n" t
-   in (Vector.fromList $ map (\case {'#' -> True; _ -> False}) $ Text.unpack algo, parse2DGrid (\c -> if c == '#' then True else False) image, False)
+      grid = parse2DGrid id image
+   in ( toBitInteger $ map (\case '#' -> True; _ -> False) $ Text.unpack algo,
+        Map.keysSet (Map.filter (== '#') grid),
+        getBounds (Map.keys grid),
+        False
+      )
 
 -- * Generics
 
-enhance (algo, image, stepParity) = (algo,,not stepParity) $
-  Map.fromList $ do
-    let (V2 xMin yMin, V2 xMax yMax) = getBounds (Map.keys image)
+toBitInteger :: [Bool] -> Integer
+toBitInteger l = foldl' f zeroBits (zip [0 ..] l)
+  where
+    f bitset (i, b)
+      | b = bitset .|. bit i
+      | otherwise = bitset
 
+enhance :: (Integer, Set (V2 Int), (V2 Int, V2 Int), Bool) -> (Integer, Set (V2 Int), (V2 Int, V2 Int), Bool)
+enhance (algo, image, bounds, stepParity) = (algo,,newBounds,not stepParity) $
+  Set.fromList $ do
     x <- [xMin - 1 .. xMax + 1]
     y <- [yMin - 1 .. yMax + 1]
 
     let coord = V2 x y
 
-    pure (coord, getPixelValue coord (algo, image, stepParity))
+    guard $ getPixelValue coord (algo, image, bounds, stepParity)
+
+    pure coord
+  where
+    newBounds = (V2 (xMin - 1) (yMin -1), V2 (xMax + 1) (yMax + 1))
+    (V2 xMin yMin, V2 xMax yMax) = bounds
 
 toBinary = foldl' f 0
   where
     f acc v = acc * 2 + bool 0 1 v
 
-getPixelValue coord (algo, image, stepParity) = (Vector.!) algo offset
+inBound (V2 xMin yMin, V2 xMax yMax) (V2 x y) = x >= xMin && x <= xMax && y >= yMin && y <= yMax
+
+getPixelValue coord (algo, image, bounds, stepParity) = testBit algo offset
   where
     offset = toBinary $ do
       dy <- [-1 .. 1]
       dx <- [-1 .. 1]
-      let delta = V2 dx dy
-      let v = case Map.lookup (coord + delta) image of
-            -- A case which does not exists is considered off or on, depending
-            -- on the parity of the step
-            Nothing -> (stepParity && (Vector.!) algo 0)
-            -- If it exists, it does have a value
-            Just r -> r
+      let coord' = coord + V2 dx dy
+      let v
+            | Set.member coord' image = True
+            | not (inBound bounds coord') && stepParity && testBit algo 0 = True
+            | otherwise = False
       pure v
 
 display g = display2DGrid (bool "." "#" <$> g)
@@ -54,12 +71,12 @@ display g = display2DGrid (bool "." "#" <$> g)
 -- * FIRST problem
 
 day :: _ -> Int
-day = length . Map.filter id . Control.Lens.view _2 . applyN 2 enhance
+day = length . Control.Lens.view _2 . applyN 2 enhance
 
 -- * SECOND problem
 
 day' :: _ -> Int
-day' = length . Map.filter id . Control.Lens.view _2 . applyN 50 enhance
+day' = length . Control.Lens.view _2 . applyN 50 enhance
 
 -- * Tests
 
