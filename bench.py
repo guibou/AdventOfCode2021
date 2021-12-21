94b6f9b9ba85bc077290a5f0c9d0d164bc137ffb
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2021-12-21 23:40:39 +0100

    Perf: I just cheated by only accounting for the time to solve star
    Not taking into account the "examples".

--- bench.py
+++ bench.py
@@ -1,7 +1,7 @@
 import subprocess
 import re
 
-p = subprocess.run(["cabal", "run", "alltests"], capture_output=True, encoding="utf8")
+p = subprocess.run(["cabal", "run", "alltests", "--", "-m", "works"], capture_output=True, encoding="utf8")
 
 for line in re.sub(r"Day(..).*?Finished", "\\1", p.stdout, flags=re.S).split('\n'):
     if "seconds" in line:
